bf8b250bd108c8cecc96b1d9cdee5b70406994eb
kcalliga <41206787+kcalliga@users.noreply.github.com>
2021-04-15 18:43:34 -0400

    Update 7_force_kube_controllermanager.sh

--- 7_force_kube_controllermanager.sh
+++ 7_force_kube_controllermanager.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-set -x
+#set -x
 
 #This will force redeployment of kube-controllermanager and check that everything is at same release
 
 DATE=`date +%m%d%y%H%M%S`;
 
-#oc patch kubecontrollermanager/cluster  --patch '{"spec":{"forceRedeploymentReason": "recovery-'"$DATE"'"}}' --type=merge
+oc patch kubecontrollermanager/cluster  --patch '{"spec":{"forceRedeploymentReason": "recovery-'"$DATE"'"}}' --type=merge
 
 
-#echo "Waiting for Operator to Update Revision Number"
+echo "Waiting for Operator to Update Revision Number"
 
-#wait 60;
+wait 60;
 
 KUBEControllerRevision=`oc describe kubecontrollermanager cluster|grep "Latest Available Revision"|awk -F ":" '{print $2}'|tr -d ' '`;
 NumberAtCurrentRevision=`oc describe kubecontrollermanager cluster|grep "Current Revision"|grep -v $KUBEControllerRevision | wc -l`;
