89efd60b354dd1915197490b364bb3a32f1bd1f5
kcalliga <41206787+kcalliga@users.noreply.github.com>
2021-04-15 18:44:05 -0400

    Update 6_force_kubeapi_update.sh

--- 6_force_kubeapi_update.sh
+++ 6_force_kubeapi_update.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-set -x
+#set -x
 
 #This will force redeployment of kube-apiserver and check that everything is at same release
 
 DATE=`date +%m%d%y%H%M%S`;
 
 oc patch kubeapiserver/cluster  --patch '{"spec":{"forceRedeploymentReason": "recovery-'"$DATE"'"}}' --type=merge
+
+echo "Wait for Operator to Update"
+
+wait 60
 
 KUBEAPIRevision=`oc describe kubeapiserver cluster|grep "Latest Available Revision"|awk -F ":" '{print $2}'|tr -d ' '`;
 NumberAtCurrentRevision=`oc describe kubeapiserver cluster|grep "Current Revision"|grep -v $KUBEAPIRevision | wc -l`;
